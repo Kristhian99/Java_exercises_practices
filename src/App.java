f046099332f0d3e05b231efafe311e587039dccf
CRISTHIAN CC <82843790+Kristhian99@users.noreply.github.com>
2021-07-17 16:33:45 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -11,8 +11,7 @@ public class App {
          
         String cadenaResultante="";
          
-        //true = Lo pasamos todo a mayusculas
-        //false= Lo pasamos todo a minusculas
+      
         boolean isMayus;
         final int DIFERENCIA=32;
          
@@ -33,37 +32,30 @@ public class App {
          
         for(int i=0;i<texto.length();i++){
              
-            //obtenemos el caracter de la pos i
+            
             caracterActual=texto.charAt(i);
              
             if(isMayus){
-                 
-                //Si esta entre esos valores, lo cambia a mayucula
-                // Puedes sustituir 97 por 'a' y asi con todos
+                
                 if((caracterActual>=97 && caracterActual<=122) || caracterActual==241 ){
                     cadenaResultante+=(char)(caracterActual-DIFERENCIA);
                 }else{
-                    //Si no es un caracter alfabetico en minuscula, lo agregamos
-                    // sin hacerle nada
+                  da
                     cadenaResultante+=caracterActual;
                 }
                  
             }else{
-                 
-                //Si esta entre esos valores, lo cambia a minuscula
-                // Puedes sustituir 65 por 'A' y asi con todos
+               
                 if((caracterActual>=65 && caracterActual<=90) || caracterActual==209){
                     cadenaResultante+=(char)(caracterActual+DIFERENCIA);
                 }else{
-                    //Si no es un caracter alfabetico en minuscula, lo agregamos
-                    // sin hacerle nada
+                    
                     cadenaResultante+=caracterActual;
                 }
                  
             }
         }
          
-        //Mostramos la cadena resultante
         JOptionPane.showMessageDialog(null, 
                                         cadenaResultante, 
                                         "Resultado", 
